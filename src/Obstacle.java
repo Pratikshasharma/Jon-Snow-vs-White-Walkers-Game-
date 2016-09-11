443e9096d0e02c62088ada920e8f99acdd285828
Pratiksha Sharma <ps179@duke.edu>
2016-09-10 21:25:40 -0400

    Added obstacle Class

---
+++ src/Obstacle.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Obstacle {
+	private ImageView myBlock;
+	private Obstacle obstacle;
+	private List<Obstacle> myBlocksList = new ArrayList<Obstacle>();
+
+	public Obstacle() {
+		myBlock = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("Block.gif")));
+	}
+
+	/**
+	 * Creates and returns blocks in Level2
+	 * @param myRoot
+	 * @return
+	 */
+	public List<Obstacle> createBlockNode(Group myRoot) {
+		for (int i = 0; i < 5; i++) {
+			obstacle = new Obstacle();
+			obstacle.getImageView().setX(Game.WIDTH-100*i-i*50);
+			obstacle.getImageView().setY(5);
+			myRoot.getChildren().add(obstacle.getImageView());
+			myBlocksList.add(obstacle);
+		}
+		return myBlocksList;
+	}
+
+	/**
+	 * Returns the myBlock ImageView of the Obstacle
+	 */
+	public ImageView getImageView() {
+		return myBlock;
+	}
+
+}
